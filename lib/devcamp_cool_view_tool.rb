9181e6e09773c834a1ad62f97b3b3c44c4ab27c8
Yurii Fulmes <yuriifu@tahometer.com>
2019-04-18 10:58:12 +0300

    Dev cool tool initial commit

---
+++ lib/devcamp_cool_view_tool.rb
@@ -0,0 +1,7 @@
+require "devcamp_cool_view_tool/version"
+require "devcamp_cool_view_tool/renderer"
+
+module DevcampCoolViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/devcamp_cool_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DevcampCoolViewTool
+  VERSION = "0.1.0"
+end
